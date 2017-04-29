1d31b45171d6c4f7e97971871c41ba661946d7d5
Michael Olson <gilfaer@gmail.com>
2017-04-29 11:27:44 -0400

    Added the RelayCommand class.

---
+++ MyToDoList/ViewModels/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyToDoList.ViewModels
+{
+    //Modified from http://stackoverflow.com/questions/22285866/why-relaycommand and
+    //http://onewindowsdev.com/?p=32
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke((T) parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(!CanExecute(parameter))
+                return;
+            _execute((T) parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
